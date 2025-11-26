e5cbfd2027cdba82b226e887c0a0e0e783617989
Sabrina <sabrina_teoh@hotmail.com>
2025-11-26 21:19:47 +1300

    add placeholder category filter

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -3,17 +3,32 @@ import { getProducts } from "../lib/useProducts";
 
 export default async function ProductList() {
   const products = await getProducts();
+  const categories = products.map((product) => product.category);
+  const uniqueCategories = [...new Set(categories)];
+
   return (
     <>
-      <h1>Product List</h1>
-      <div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <ul>
-            {products.map((product) => (
-              <li key={product.id}>{product.title}</li>
+      <div className="flex flex-col p-10">
+        <h1 className="text-5xl">Product List</h1>
+        <div>
+          <label htmlFor="category-select">Category Filter:</label>
+          <select name="category" id="category-select">
+            {uniqueCategories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
             ))}
-          </ul>
-        </Suspense>
+          </select>
+        </div>
+        <div className="p-10">
+          <Suspense fallback={<div>Loading...</div>}>
+            <ul>
+              {products.map((product) => (
+                <li key={product.id}>{product.title}</li>
+              ))}
+            </ul>
+          </Suspense>
+        </div>
       </div>
     </>
   );
--- lib/useProducts.ts
+++ lib/useProducts.ts
@@ -1,10 +1,18 @@
-export async function getProducts(): Promise<{ id: string; title: string }[]> {
+export async function getProducts(): Promise<
+  { id: string; title: string; category: string }[]
+> {
   const jsonData = await fetch("http://localhost:3000/api/products").then((r) =>
     r.json()
   );
   const products = jsonData.products;
 
-  return products.map((product: { id: string; title: string }) => {
-    return { id: product.id, title: product.title };
-  });
+  return products.map(
+    (product: { id: string; title: string; category: string }) => {
+      return {
+        id: product.id,
+        title: product.title,
+        category: product.category,
+      };
+    }
+  );
 }
